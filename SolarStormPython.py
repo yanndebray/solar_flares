4ac7254fdbf89d31fe2968d089f22d7e44be1fb2
Maria Elena Gavilan <50922741+MariaGavilan@users.noreply.github.com>
2025-02-27 09:37:44 -0500

    Adding latest updates to my project!

--- SolarStormPython.py
+++ SolarStormPython.py
@@ -13,25 +13,25 @@ tstart = "2024/05/07"
 tend = "2024/05/15"
 result = Fido.search(a.Time(tstart, tend),
                      a.hek.EventType(event_type),
-                     a.hek.FL.GOESCls > "M3.0",
+                     a.hek.FL.GOESCls > "C3.0",
                      a.hek.OBS.Observatory == "GOES")
 
 # Here we only show two columns due there being over 
 # 100 columns returned normally.
-print(result.show("hpc_bbox", "refs"))
+#print(result.show("hpc_bbox", "refs"))
 
 # It"s also possible to access the HEK results from the
 # `~sunpy.net.fido_factory.UnifiedResponse` by name.
 hek_results = result["hek"]
 
 # We only print every 10th key to avoid the output being too long.
-print(hek_results.colnames[::10])
+#print(hek_results.colnames[::10])
 
 filtered_results = hek_results["event_starttime", "event_peaktime",
                                "event_endtime", "fl_goescls", 'hpc_radius',
                                "ar_noaanum"]
 
-print(type(filtered_results))
+#print(type(filtered_results))
 
 results_pandas = filtered_results.to_pandas()
 print(type(results_pandas))
@@ -46,7 +46,28 @@ by_magnitude = sorted(filtered_results,
 for flare in by_magnitude:
     print(f"Class {flare['fl_goescls']} occurred on {flare['event_starttime']}")
 
+# Let’s grab GOES XRS data for a particular time of interest and the 
+# HEK flare data for this time from NOAA. 
+# Here we are searching for data from the GOES-16 satellite 
+# and for the 1-min average time-sampled data.
+tr = a.Time('2024-05-11 01:00', '2024-05-11 23:00')
+results16 = Fido.search(tr, a.Instrument.xrs & a.goes.SatelliteNumber(16) & a.Resolution("avg1m") | a.hek.FL & (a.hek.FRM.Name == 'SWPC'));
 
+# We will now use TimeSeries data
+from sunpy.timeseries import TimeSeries
+
+## Additional code to prevent warning messages in Windows
+import asyncio
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+# Download the XRS data and load it into a TimeSeries, convert to a 
+# dataframe for easier manipulation, and find the max values
+files = Fido.fetch(results16)
+goes = TimeSeries(files)
+goes_data = goes.to_dataframe()
+
+max_xrsa = goes_data['xrsa'].max()
+max_xrsb = goes_data['xrsb'].max()
 
 
 
